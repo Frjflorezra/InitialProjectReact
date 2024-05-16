b9184e2d040d706944753963fa4e0d57d538b433
Frjflorezra <franciscojflorezr@gmail.com>
2024-05-15 20:57:45 -0500

    use of useState and event handling

--- src/Component.js
+++ src/Component.js
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+ 
 export default function Component() {
+  const [text, setText] = useState()
+  const [updated, setUpdated] = useState()
+
+  const buttonOnClick = () => { 
+    setUpdated(text)
+   }
+
+  const textOnChange = (event) => {
+    setText(event.target.value)
+  }
+
   return (
-    <div>My Component</div>
+    <div>
+      <input type='text' value={text} onChange={textOnChange}></input>
+      <button onClick={buttonOnClick}>Actualizar</button>
+      <p>Texto input: {text}</p>
+      <p>Texto Actualizado: {updated}</p>
+    </div>
   )
 }
